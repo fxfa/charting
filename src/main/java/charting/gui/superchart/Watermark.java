b3b81829d6cab4d9381a7684d7152fe996e6d90c
fxfa <f4b1999@gmail.com>
2024-03-10 15:07:14 +0100

    Fix Watermark text centering

--- src/main/java/charting/gui/superchart/Watermark.java
+++ src/main/java/charting/gui/superchart/Watermark.java
@@ -8,6 +8,7 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.geometry.Bounds;
 import javafx.geometry.VPos;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -23,6 +24,8 @@ public class Watermark implements Drawing {
             new SimpleObjectProperty<>(new Color(0.5, 0.5, 0.5, 0.15));
     private final StringProperty shortName = new SimpleStringProperty();
     private final StringProperty longName = new SimpleStringProperty();
+
+    private final javafx.scene.text.Text measureText = new javafx.scene.text.Text();
 
     public Watermark() {
     }
@@ -77,13 +80,38 @@ public class Watermark implements Drawing {
             return Collections.emptyList();
         }
 
-        Text shortNameText = new Text(getShortName(), drawingContext.getViewport().getCenterX(),
+        Text shortText = new Text(getShortName(), drawingContext.getViewport().getCenterX(),
                 drawingContext.getViewport().getCenterY(), getColor(), SHORT_NAME_FONT);
-        shortNameText.setBaseline(longNameEmpty ? VPos.CENTER : VPos.BOTTOM);
-        Text longNameText = new Text(getLongName(), drawingContext.getViewport().getCenterX(),
+        Text longText = new Text(getLongName(), drawingContext.getViewport().getCenterX(),
                 drawingContext.getViewport().getCenterY(), getColor(), LONG_NAME_FONT);
-        longNameText.setBaseline(shortNameEmpty ? VPos.CENTER : VPos.TOP);
 
-        return List.of(shortNameText, longNameText);
+        if (shortNameEmpty) {
+            return List.of(longText);
+        }
+
+        if (longNameEmpty) {
+            return List.of(shortText);
+        }
+
+        shortText.setBaseline(VPos.TOP);
+        longText.setBaseline(VPos.TOP);
+
+        Bounds shortBounds = getTextBounds(shortText);
+        Bounds longBounds = getTextBounds(longText);
+
+        double y = drawingContext.getCanvasHeight() / 2 - (shortBounds.getHeight() + longBounds.getHeight()) / 2;
+
+        shortText.setY(drawingContext.toViewportY(y));
+        longText.setY(drawingContext.toViewportY(y + shortBounds.getHeight()));
+
+        return List.of(shortText, longText);
+    }
+
+    private Bounds getTextBounds(Text text) {
+        measureText.setText(text.getText());
+        measureText.setFont(text.getFont());
+        measureText.setTextOrigin(text.getBaseline());
+        measureText.setTextAlignment(text.getAlignment());
+        return measureText.getLayoutBounds();
     }
 }
